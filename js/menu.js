c73a38af956abe75ca29fed94e5f54ca72f80391
Piero Mendieta <114272608+PieroMS@users.noreply.github.com>
2023-08-02 09:45:13 -0500

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+(function(){
+    const button = document.querySelector('.button');
+    const nav = document.querySelector('.nav');
+    button.addEventListener('click',()=>{
+        nav.classList.toggle('active');
+    });
+    const li1 = document.querySelector('.li1');
+    li1.addEventListener('click',()=>{
+        nav.classList.toggle('active');
+    });
+    const li2 = document.querySelector('.li2');
+    li2.addEventListener('click',()=>{
+        nav.classList.toggle('active');
+    });
+    const li3 = document.querySelector('.li3');
+    li3.addEventListener('click',()=>{
+        nav.classList.toggle('active');
+    });
+})();
